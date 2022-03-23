c396c0eb4dd6be514aeb7c308f0b390b93e4ea46
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-03-23 22:00:42 +0300

    Refactoring

--- src/back-end/microservices/IdentityService/Infrastructure/ServiceCollectionExtensions.cs
+++ src/back-end/microservices/IdentityService/Infrastructure/ServiceCollectionExtensions.cs
@@ -7,8 +7,12 @@ public static class ServiceCollectionExtensions
     public static void AddInfrastructure(this IServiceCollection serviceProvider, IConfiguration configuration,
         IWebHostEnvironment environment)
     {
+        #region Register DbContext
+
         var dbConnectionSectionName = environment.IsDevelopment() ? "DevelopDbConnection" : "ReleaseDbConnection";
         serviceProvider.AddDbContext<ApplicationDbContext>(builder =>
             builder.UseSqlServer(configuration.GetConnectionString(dbConnectionSectionName)));
+
+        #endregion
     }
 }
