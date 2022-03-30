a84d59efd1711c8ba4a8e76642345708972cb7be
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-03-30 21:48:14 +0300

    Add AutoMapper

--- src/back-end/microservices/IdentityService/Application/ApplicationDependencyInjection.cs
+++ src/back-end/microservices/IdentityService/Application/ApplicationDependencyInjection.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace IdentityService.Application;
 
 public static class ApplicationDependencyInjection
@@ -5,6 +7,6 @@ public static class ApplicationDependencyInjection
     public static void AddApplication(this IServiceCollection serviceProvider, IConfiguration configuration,
         IWebHostEnvironment environment)
     {
-        
+        serviceProvider.AddAutoMapper(Assembly.GetExecutingAssembly());
     }
 }
---
+++ src/back-end/microservices/IdentityService/Application/Mapper/MapperExtensions.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+
+namespace IdentityService.Application.Mapper;
+
+public static class MapperExtensions
+{
+    private static MapperConfiguration _mapperConfiguration;
+    
+    static MapperExtensions()
+    {
+        // _mapperConfiguration = new MapperConfiguration(x => x.)
+    }
+}
