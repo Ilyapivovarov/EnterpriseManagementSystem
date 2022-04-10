1ee456c9e213b36ba71bcf1275961a5a56b62c64
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-04-10 20:07:50 +0300

    Refactoring

--- src/front-end/react-client-app/src/components/layout/index.tsx
+++ src/front-end/react-client-app/src/components/layout/index.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Container } from "reactstrap";
+import NavMenu from "../nav";
 
 const Layout: React.FC = () => (
-    <Container>
-        <Outlet />
+  <>
+    <NavMenu />
+    <Container fluid>
+      <Outlet />
     </Container>
+  </>
 );
 
 export default Layout;
