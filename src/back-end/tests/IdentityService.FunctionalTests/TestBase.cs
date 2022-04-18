8376df7e0052d36d5197d9df7d13dbb070389b62
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-04-18 09:12:44 +0300

    Refactoring

--- src/back-end/tests/IdentityService.FunctionalTests/TestBase.cs
+++ src/back-end/tests/IdentityService.FunctionalTests/TestBase.cs
@@ -2,15 +2,13 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
+using System.Text.Json;
 using IdentityService.Application.Models;
 using IdentityService.Infrastructure.AppData;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 
 namespace IdentityService.FunctionalTests;
@@ -19,37 +17,27 @@ public class TestBase
 {
     public TestBase()
     {
-        EmptyServer = GetEmptyTestServer();
         User = GetUser();
+        Server = GetTestServer();
+        JsonSerializerOptions = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
     }
-
+    
     protected User User { get; }
     
-    private TestServer EmptyServer { get; }
+    protected  TestServer Server { get; }
+    
+    protected  JsonSerializerOptions JsonSerializerOptions { get; }
+    
+    protected string? AccessToken { get; set; }
 
     [OneTimeTearDown]
     public void RemoveDatabase()
     {
-        var context = EmptyServer.Services.GetRequiredService<ApplicationDbContext>();
+        var context = Server.Services.GetRequiredService<ApplicationDbContext>();
         context.Database.EnsureDeleted();
     }
 
-    private static TestServer GetEmptyTestServer()
-    {
-        var path = Assembly.GetExecutingAssembly().Location;
-
-        var hostBuilder = new WebHostBuilder()
-            .UseContentRoot(Path.GetDirectoryName(path) ?? throw new ArgumentNullException(nameof(path)))
-            .ConfigureAppConfiguration(configuration =>
-            {
-                configuration.AddJsonFile("appsettings.json", false)
-                    .AddEnvironmentVariables();
-            }).UseStartup<Startup>();
-
-        return new TestServer(hostBuilder);
-    }
-
-    protected TestServer GetTestServer()
+    private TestServer GetTestServer()
     {
         var path = Assembly.GetExecutingAssembly().Location;
         var hostBuilder = new WebHostBuilder()
@@ -65,13 +53,12 @@ public class TestBase
         if (!context.Users.Any())
         {
             context.Users.Add(User);
-            
             context.SaveChanges();
         }
         
         return test;
     }
-
+    
     private User GetUser()
     {
         return new User()
--- src/back-end/tests/IdentityService.FunctionalTests/UserControllerTests.cs
+++ src/back-end/tests/IdentityService.FunctionalTests/UserControllerTests.cs
@@ -1,35 +1,16 @@
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using EnterpriseManagementSystem.Contracts.WebContracts;
-using IdentityService.Application.Models;
-using IdentityService.Infrastructure.AppData;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
-using Session = EnterpriseManagementSystem.Contracts.WebContracts.Session;
 
 namespace IdentityService.FunctionalTests;
 
 public class UserControllerTests : TestBase
 {
-    private readonly JsonSerializerOptions _jsonSerializerOptions;
-
-    public UserControllerTests()
-    {
-        Server = GetTestServer();
-        _jsonSerializerOptions = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
-    }
-
-    private string? AccessToken { get; set; }
-    
-    private TestServer Server { get; }
-    
     [SetUp]
     public async Task SetUp()
     {
@@ -38,7 +19,7 @@ public class UserControllerTests : TestBase
         var response = await client.PostAsync("auth/sign-in", signInContent);
         
         var sessionDraft = await response.Content.ReadAsStringAsync();
-        var sessionDto = JsonSerializer.Deserialize<Session>(sessionDraft, _jsonSerializerOptions);
+        var sessionDto = JsonSerializer.Deserialize<Session>(sessionDraft, JsonSerializerOptions);
 
         AccessToken = sessionDto?.AccessToken;
     }
@@ -46,27 +27,14 @@ public class UserControllerTests : TestBase
     [Test]
     public async Task ChangeUserInfoTest()
     {
-        var user = GetUserFromDb();
         var client = Server.CreateClient();
-        
-        var data = new UserInfo(user.Guid, "Test", "Test", "Reader");
-        var content = new StringContent(JsonSerializer.Serialize(data, _jsonSerializerOptions), Encoding.UTF8, MediaTypeNames.Application.Json);
+        var data = new UserInfo(User.Guid, "Test", "Test", "Reader");
+        var content = new StringContent(JsonSerializer.Serialize(data, JsonSerializerOptions), Encoding.UTF8, MediaTypeNames.Application.Json);
         client.DefaultRequestHeaders.Authorization =
             new AuthenticationHeaderValue("Bearer", AccessToken);
         
         var response = await client.PostAsync("user/update", content);
+        
         Assert.IsTrue(response.IsSuccessStatusCode);
-    }
-    
-    [OneTimeTearDown]
-    public new void RemoveDatabase()
-    {
-        var context = Server.Services.GetRequiredService<ApplicationDbContext>();
-        context.Database.EnsureDeleted();
-    }
-
-    private User GetUserFromDb()
-    {
-        return Server.Services.GetRequiredService<ApplicationDbContext>().Users.First();
     }
 }
