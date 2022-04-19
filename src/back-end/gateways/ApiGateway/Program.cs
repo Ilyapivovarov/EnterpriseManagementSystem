a63eef375e59a224d89729b7d17b133fa45f85cd
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-04-19 18:59:14 +0300

    Refactoring

--- src/back-end/gateways/ApiGateway/Program.cs
+++ src/back-end/gateways/ApiGateway/Program.cs
@@ -1,10 +1,5 @@
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-    .ConfigureLogging(builder =>
-    {
-        builder.ClearProviders();
-        builder.AddConsole();
-    })
     .Build();
 
 await host.RunAsync();
--- src/back-end/microservices/IdentityService/Program.cs
+++ src/back-end/microservices/IdentityService/Program.cs
@@ -2,18 +2,11 @@ using IdentityService;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-    .ConfigureLogging(builder =>
-    {
-        builder.ClearProviders();
-        builder.AddConsole();
-    })
     .Build();
-
 
 var scope = host.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<ApplicationDbContextSeed>>();
 await ApplicationDbContextSeed.SeedData(context, logger);
-
 
 await host.RunAsync();
