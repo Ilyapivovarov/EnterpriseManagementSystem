e4b7aa653e1c997197212564d6659518ee272237
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-06-24 19:05:45 +0300

    Register MassTransit

--- src/back-end/microservices/UserService/Infrastructure/InfrastructureExtensions.cs
+++ src/back-end/microservices/UserService/Infrastructure/InfrastructureExtensions.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using UserService.Application.DbContexts;
 using UserService.Infrastructure.DbContexts;
 
@@ -23,5 +24,27 @@ public static class InfrastructureExtensions
         services.AddScoped<IUserDbContext, UserDbContext>();
 
         #endregion
+
+        #region Register MassTransit
+
+        services.AddMassTransit(configurator =>
+        {
+            if (environment.IsEnvironment("Testing"))
+                configurator.UsingInMemory((context, cfg) => cfg.ConfigureEndpoints(context));
+            else
+                configurator.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host("localhost", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+        });
+
+        #endregion
+        
+        
     }
 }
