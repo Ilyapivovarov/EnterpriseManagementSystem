826f8e362642a0e8404dd67c3807c27779acfae5
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-07-27 10:14:00 +0300

    Fix

--- src/front-end/react-client-app/src/components/Layout/Layout.tsx
+++ src/front-end/react-client-app/src/components/Layout/Layout.tsx
@@ -28,27 +28,25 @@ interface AppBarProps extends MuiAppBarProps {
 }
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open'
-  })
-  // eslint-disable-next-line no-unexpected-multiline
-  < AppBarProps > (({
-    theme,
-    open
-  }) => ({
-    zIndex: theme.zIndex.drawer + 1,
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({
+  theme,
+  open
+}) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    ...(open && {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen
-      })
-    })
-  }))
+  }),
+}))
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
   ({
@@ -106,52 +104,52 @@ const DashboardContent: React.FC = () => {
           <IconButton
             edge="start"
             color="inherit"
-                        aria-label="open drawer"
-                        onClick={toggleDrawer}
-                        sx={{
-                          marginRight: '36px',
-                          ...(open && { display: 'none' })
-                        }}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography
-                      component="h1"
-                      variant="h6"
-                      color="inherit"
-                      noWrap
-                      sx={{ flexGrow: 1 }}
-                    >
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: '36px',
+              ...(open && { display: 'none' })
+            }}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
 
-                    </Typography>
-                    <IconButton color="inherit">
-                        <Badge badgeContent={4} color="secondary">
-                            <NotificationsIcon/>
-                        </Badge>
-                    </IconButton>
-                    <IconButton color="inherit" onClick={logOutHandler}>
-                        <Badge color="secondary">
-                            <LogoutIcon/>
-                        </Badge>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <Toolbar
-                    sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-end',
-                      px: [1]
-                    }}
-                >
-                  <IconButton onClick={toggleDrawer}>
-                    <ChevronLeftIcon/>
-                  </IconButton>
-                </Toolbar>
-              <Divider/>
-              <NavMenu/>
-            </Drawer>
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon/>
+            </Badge>
+          </IconButton>
+          <IconButton color="inherit" onClick={logOutHandler}>
+            <Badge color="secondary">
+              <LogoutIcon/>
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            px: [1]
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon/>
+          </IconButton>
+        </Toolbar>
+        <Divider/>
+        <NavMenu/>
+      </Drawer>
       {isLoading
         ? <Loader/>
         : <Box
@@ -171,9 +169,9 @@ const DashboardContent: React.FC = () => {
               <Outlet/>
             </Grid>
           </Container>
-                </Box>
-            }
         </Box>
+      }
+    </Box>
   )
 }
 
