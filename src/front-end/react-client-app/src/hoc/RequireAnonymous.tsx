8b2a0d8a8731c64df91510640b97258e63b91a51
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-07-31 10:18:33 +0300

    Fix hoc

--- src/front-end/react-client-app/src/hoc/RequireAnonymous.tsx
+++ src/front-end/react-client-app/src/hoc/RequireAnonymous.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
-import { useAppSelector } from '../hooks'
+import { useAppDispatch } from '../hooks'
+import { resetAuthState } from '../store/AuthReducer/AuthActionCreators'
 
 const RequireAnonymous: React.FC = () => {
-  const { currentSession } = useAppSelector(x => x.authReducer)
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
 
   React.useEffect(() => {
-    if (currentSession) {
-      navigate('/')
-    }
-  }, [currentSession])
+    dispatch(resetAuthState())
+      .unwrap()
+      .then(() => {
+        navigate('/')
+      })
+  }, [])
 
   return (
     <Outlet/>
--- src/front-end/react-client-app/src/hoc/RequireAuth.tsx
+++ src/front-end/react-client-app/src/hoc/RequireAuth.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useAppSelector } from '../hooks'
+import { useAppDispatch, useAppSelector } from '../hooks'
+import { resetAuthState } from '../store/AuthReducer/AuthActionCreators'
 
 const RequireAuth: React.FC = (props) => {
   const navigate = useNavigate()
   const { currentSession } = useAppSelector(x => x.authReducer)
 
+  const dispatch = useAppDispatch()
+
   React.useEffect(() => {
-    if (!currentSession) {
-      navigate('sign-in')
-    }
-  }, [currentSession])
+    dispatch(resetAuthState())
+      .unwrap()
+      .catch(() => {
+        navigate('/sign-in')
+      })
+  }, [])
 
   if (currentSession) {
     return (
