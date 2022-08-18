a23cdb7b5372501133f680183fd2a4cb9b7a0286
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-08-18 17:34:32 +0300

    Update TaskList.tsx

--- src/front-end/react-client-app/src/pages/TaskListPage/index.tsx
+++ src/front-end/react-client-app/src/pages/TaskListPage/index.tsx
@@ -3,30 +3,48 @@ import PageWrapper from '../../components/PageWrapper/PageWrapper';
 import TaskList from '../../components/TaskList/TaskList';
 import {Breadcrumbs, Paper, Typography} from '@mui/material';
 import Link from '../../components/Link/Link';
+import {useNavigate} from 'react-router-dom';
+import {useGetTasksByPageQuery} from '../../services/taskService';
+import Loader from '../../components/Loader/Loader';
 
 const TaskListPage: React.FC = () => {
-  return (
-    <>
-      <Paper
-        sx={{
-          p: 2,
-          marginTop: '10px',
-        }}
-        elevation={1}>
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link to={'/'}>
-            Home
-          </Link>
-          <Typography color="text.primary">Tasks</Typography>
-        </Breadcrumbs>
-      </Paper>
+  const navigate = useNavigate();
 
-      <PageWrapper>
-        <TaskList/>
-      </PageWrapper>
-    </>
+  const [page, setPage] = React.useState<number>(1);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  );
+  const {data, error, isSuccess, isLoading} = useGetTasksByPageQuery({pageNumber: page, pageSize: rowsPerPage});
+
+  if (isLoading) {
+    return <Loader/>;
+  }
+
+  if (isSuccess) {
+    console.log(data);
+    return (
+      <>
+        <Paper
+          sx={{
+            p: 2,
+            marginTop: '10px',
+          }}
+          elevation={1}>
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link to={'/'}>
+                            Home
+            </Link>
+            <Typography color="text.primary">Tasks</Typography>
+          </Breadcrumbs>
+        </Paper>
+
+        <PageWrapper>
+          <TaskList tasks={data}/>
+        </PageWrapper>
+      </>
+    );
+  }
+
+  return <>{JSON.parse(JSON.stringify(error)).data}</>;
 };
 
 export default TaskListPage;
