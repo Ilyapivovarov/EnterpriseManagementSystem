0b02c20130be12bb88ecf88bc47c232d927e498a
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-08-24 20:50:58 +0300

    TaskStatusSelector.tsx

--- src/front-end/react-client-app/src/components/TaskStatusSelector/TaskStatusSelector.tsx
+++ src/front-end/react-client-app/src/components/TaskStatusSelector/TaskStatusSelector.tsx
@@ -30,7 +30,7 @@ const TaskStatusSelector: FC<TaskStatusSelectorProps> = ({status, onChange}) => 
   if (isSuccess) {
     return (
       <Box p={1} sx={{minWidth: 120}}>
-        <FormControl variant="standard" >
+        <FormControl fullWidth variant="standard" >
           <InputLabel id="task-status-select">Status</InputLabel>
           <Tooltip title={'Change status'} placement="top">
             <Select
