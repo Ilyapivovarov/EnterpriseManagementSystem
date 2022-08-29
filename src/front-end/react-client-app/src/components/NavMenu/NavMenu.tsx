96e7c393f835e1ffa6d19ad0b50573dbb97467d9
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-08-29 21:27:07 +0300

    Update NavMenu.tsx

--- src/front-end/react-client-app/src/components/NavMenu/NavMenu.tsx
+++ src/front-end/react-client-app/src/components/NavMenu/NavMenu.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import DashboardIcon from '@mui/icons-material/Dashboard';
 import ListItemText from '@mui/material/ListItemText';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import List from '@mui/material/List';
 import AdminNavItems from '../AdminNavItems/AdminNavItems';
 import {NavLink} from 'react-router-dom';
+import PeopleIcon from '@mui/icons-material/People';
+import ListAltIcon from '@mui/icons-material/ListAlt';
 
 const NavMenu: React.FC = () => {
   return (
     <List component="nav">
-      <ListItemButton component={NavLink} to={'/users'}>
+      <ListItemButton component={NavLink} to={'/employees'}>
         <ListItemIcon>
-          <DashboardIcon/>
+          <PeopleIcon/>
         </ListItemIcon>
-        <ListItemText primary="Users"/>
+        <ListItemText primary="Employees"/>
       </ListItemButton>
       <ListItemButton component={NavLink} to={'/tasks'}>
         <ListItemIcon>
-          <ShoppingCartIcon/>
+          <ListAltIcon/>
         </ListItemIcon>
         <ListItemText primary="Tasks">
         </ListItemText>
