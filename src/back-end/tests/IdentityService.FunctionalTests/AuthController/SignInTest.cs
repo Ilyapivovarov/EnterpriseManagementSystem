8f64f3460578e0b9581b91a9ba7aa084f0e45ff5
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-09-07 15:45:52 +0300

    Fix

--- src/back-end/tests/IdentityService.FunctionalTests/AuthController/SignInTest.cs
+++ src/back-end/tests/IdentityService.FunctionalTests/AuthController/SignInTest.cs
@@ -2,7 +2,6 @@ using System.Net;
 using System.Threading.Tasks;
 using EnterpriseManagementSystem.BusinessModels;
 using EnterpriseManagementSystem.Contracts.Dto.IdentityServiceDto;
-using EnterpriseManagementSystem.Contracts.WebContracts;
 using IdentityService.FunctionalTests.Base;
 using NUnit.Framework;
 
@@ -10,8 +9,8 @@ namespace IdentityService.FunctionalTests.AuthController;
 
 public sealed class SignInTest : TestBase
 {
-    protected override string EnvironmentName => "Development";
-    
+    protected override string EnvironmentName => "Testing";
+
     [SetUp]
     public async Task Setup()
     {
@@ -21,8 +20,8 @@ public sealed class SignInTest : TestBase
     [Test]
     public async Task SuccessScenario()
     {
-        var data = new SignInDto(EmailAddress.Parse("admin@admin.com"), Password.Parse("admin"));
-        
+        var data = new SignInDto(EmailAddress.Parse("admin@ems.com"), Password.Parse("admin"));
+
         var result = await Client.PostAsync("auth/sign-in", GetStringContent(data));
 
         Assert.IsTrue(result.IsSuccessStatusCode, await result.Content.ReadAsStringAsync());
@@ -32,7 +31,7 @@ public sealed class SignInTest : TestBase
     public async Task IncrrectEmailOrPasswordScenario()
     {
 
-        var data = new SignInDto(EmailAddress.Parse("admin@admin.com"), Password.Parse("asfasfas"));
+        var data = new SignInDto(EmailAddress.Parse("admin@ems.com"), Password.Parse("asfasfas"));
 
         var result = await Client.PostAsync("auth/sign-in", GetStringContent(data));
 
