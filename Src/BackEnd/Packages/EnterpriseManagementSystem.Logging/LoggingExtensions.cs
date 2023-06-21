1f95ba6d72b846781e331eba709275816872cd3e
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2023-06-21 11:36:14 +0300

    Added console logger

--- Src/BackEnd/Packages/EnterpriseManagementSystem.Logging/LoggingExtensions.cs
+++ Src/BackEnd/Packages/EnterpriseManagementSystem.Logging/LoggingExtensions.cs
@@ -15,6 +15,7 @@ public static class LoggingExtensions
         {
             builder.ClearProviders();
             builder.AddDbLogger();
+            builder.AddConsole();
         });
     }
     
