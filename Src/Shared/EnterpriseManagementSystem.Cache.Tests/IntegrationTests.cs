21e76b828a9f898220e296074cee35ef069dc820
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2024-03-16 15:47:14 +0500

    Init EnterpriseManagementSystem.Cache.Tests

---
+++ Src/Shared/EnterpriseManagementSystem.Cache.Tests/IntegrationTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EnterpriseManagementSystem.Cache.Tests;
+
+public class IntegrationTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Src/Shared/EnterpriseManagementSystem.Cache.Tests/UnitTests.cs
@@ -0,0 +1,15 @@
+namespace EnterpriseManagementSystem.Cache.Tests;
+
+public class UnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
