b411ed42445db4d37958ef79231ec4947d114f94
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2024-03-22 14:58:23 +0500

    Add LoggingServiceContext and LogDbEntity to LoggingService

---
+++ Src/BackEnd/Services/LoggingService/AppContext/Entities/LogDbEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoggingService.AppContext.Entities;
+
+public class LogDbEntity
+{
+    public int Id { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Uid { get; protected set; } = Guid.NewGuid();
+
+    public LogLevel Level { get; set; }
+
+    public required string AppName { get; set; }
+    
+    public required string Message { get; set; }
+
+    public string? Exception { get; set; }
+}
---
+++ Src/BackEnd/Services/LoggingService/AppContext/LoggingServiceContext.cs
@@ -0,0 +1,13 @@
+﻿using LoggingService.AppContext.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoggingService.AppContext;
+
+public class LoggingServiceContext : DbContext 
+{
+    public LoggingServiceContext(DbContextOptions<LoggingServiceContext> options)
+        : base(options)
+    { }
+    
+    public DbSet<LogDbEntity> Logs => Set<LogDbEntity>();
+}
