b9977c62037f2ae8c173bf91224d5fcb4a20fdaf
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2024-03-22 14:40:47 +0500

    Create LoggingService

--- Src/BackEnd/Services/LogWriterService/_Imports.cs
+++
@@ -1,7 +0,0 @@
-﻿// Global using directives
-
-global using EnterpriseManagementSystem.Contracts.Messages;
-global using EnterpriseManagementSystem.MessageBroker.Abstractions;
-global using LogWriterService.Application.DbContexts;
-global using LogWriterService.Core.DbEntities;
-global using Microsoft.EntityFrameworkCore;
